6d424e7fa39219daf10c7f9d9f17b91335b5e23f
Yoshiaki Satoh <y-sato@mediba.jp>
2016-06-08 20:29:09 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var webPage = require('webpage');
+var page = webPage.create();
+
+system = require('system');
+
+if (system.args.length === 1) {
+console.log('Usage: index.js <some URL>');
+phantom.exit();
+}
+
+address = system.args[1];
+page.viewportSize = { width: 320 , height: 480}
+page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A365 Safari/600.1.4'
+page.open(address, function(status) {
+    console.log("status = " + status);
+
+    if (status === "success") {
+        page.evaluate(function() {
+          document.body.bgColor = 'white';
+        })
+        window.setTimeout(function() {
+          page.render("ss.png");
+          phantom.exit();
+        },1000)
+    }
+});
