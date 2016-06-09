089cb48f06a6dfb41cc37e162849f29f78dc012d
Yoshiaki Satoh <y-sato@mediba.jp>
2016-06-09 20:29:54 +0900

    useragent定義のJSONを外部ファイルに追い出し

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ var webPage = require('webpage');
 var page = webPage.create();
 
 system = require('system');
-
-var userAgents = {
-  'iphone6' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A365 Safari/600.1.4',
-  'default' : 'Mozilla/5.0 (Linux; U; Android 1.5; ja-jp; GDDJ-09 Build/CDB56) AppleWebKit/528.5+ (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1'
-}
+var userAgents = require('./useragent.json')
 var ua, address, outputFileName;
-
 
 if (system.args.length === 1) {
   console.log('Usage: index.js <some URL>');
