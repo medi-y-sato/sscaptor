199db469a8c1d01ef78fdf9f952f6d21fdae1c5a
Yoshiaki Satoh <y-sato@mediba.jp>
2016-06-09 09:21:17 +0900

    画像のクオリティを明示

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ var page = webPage.create();
 system = require('system');
 
 if (system.args.length === 1) {
-console.log('Usage: index.js <some URL>');
-phantom.exit();
+  console.log('Usage: index.js <some URL>');
+  phantom.exit();
 }
 
 address = system.args[1];
-page.viewportSize = { width: 320 , height: 480}
-page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A365 Safari/600.1.4'
-page.open(address, function(status) {
-    console.log("status = " + status);
 
-    if (status === "success") {
-        page.evaluate(function() {
-          document.body.bgColor = 'white';
-        })
-        window.setTimeout(function() {
-          page.render("ss.png");
-          phantom.exit();
-        },1000)
-    }
+page.viewportSize = {
+  width: 320,
+  height: 480
+}
+page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A365 Safari/600.1.4'
+
+page.open(address, function(status) {
+  console.log("status = " + status);
+
+  if (status === "success") {
+    page.evaluate(function() {
+      document.body.bgColor = 'white';
+    })
+    window.setTimeout(function() {
+      page.render("ss.png",{format: 'png', quality: '100'});
+      phantom.exit();
+    }, 1000)
+  }
 });
